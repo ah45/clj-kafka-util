78d2596dfd198ffb3be220bfb4958cec0b837602
Adam Harper <github@adam-harper.com>
2015-11-11 13:27:56 +0000

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.ah45/clj-kafka-util "0.1.0"
+(defproject org.clojars.ah45/clj-kafka-util "0.1.1"
   :description "A collection of utility fns related to clj-kafka"
   :url "https://github.com/ah45/clj-kafka-util"
   :license {:name "Eclipse Public License"
